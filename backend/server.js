32969992df0946b9df597534520feb875b1c15ab
wisehackermonkey <oranbusiness@gmail.com>
2019-04-14 02:13:22 -0700

    working button, backend, and start of the css

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,29 @@
  github.com/wisehackermonkey
  20190413
 */
-console.log("Big red button server");
-console.log("started");
-// setTimeout(() => {
-//     console.log("works");
-// }, 1000);
+import express from 'express';
 
-import request from 'requests';
-let helloWorld = 'Hello World!';
-console.log(`${helloWorld} this is some ES6 JavaScript code`);
+console.log("Big red button server")
+console.log("started")
+
+const app = express()
+const port = 3000
+let current = 50;
+app.use(express.static("public"))
+
+app.options('/api/current', function (req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.end();
+});
+app.get('/status', (request, response) => {
+    current+=1
+    response.send(current.toString())
+})
+
+app.get('/load', (request, response) => {
+    response.send(current.toString())
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
