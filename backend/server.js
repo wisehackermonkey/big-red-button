22f29223cebbbf4e63b0c60582c6fab45c48a20d
wisehackermonkey <oranbusiness@gmail.com>
2019-04-13 21:44:49 -0700

    created blank backend, added es6 build command, npm init'ed

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+/*
+ Big red button backend server
+ by Oran C
+ oranbusiness@gmail.com
+ github.com/wisehackermonkey
+ 20190413
+*/
+console.log("Big red button server");
+console.log("started");
+// setTimeout(() => {
+//     console.log("works");
+// }, 1000);
+
+import request from 'requests';
+let helloWorld = 'Hello World!';
+console.log(`${helloWorld} this is some ES6 JavaScript code`);
